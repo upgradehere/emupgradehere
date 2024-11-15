31e0adebea1d6191b4a96920788923ad8c58e827
ilman30 <ilmannurhakim0@gmail.com>
2024-11-15 17:06:35 +0700

    <improve> add dashboard

---
+++ resources/views/templates/template.blade.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EMHEALTH</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('templates/adminlte-3.2.0/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet"
+        href="{{ asset('templates/adminlte-3.2.0/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet"
+        href="{{ asset('templates/adminlte-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{ asset('templates/adminlte-3.2.0/plugins/jqvmap/jqvmap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('templates/adminlte-3.2.0/dist/css/adminlte.min.css') }}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet"
+        href="{{ asset('templates/adminlte-3.2.0/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset('templates/adminlte-3.2.0/plugins/daterangepicker/daterangepicker.css') }}">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('templates/adminlte-3.2.0/plugins/summernote/summernote-bs4.min.css') }}">
+    <!-- DataTables -->
+    <link rel="stylesheet"
+        href="{{ asset('templates/adminlte-3.2.0/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('templates/adminlte-3.2.0/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('templates/adminlte-3.2.0/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
+    <div class="wrapper">
+
+        <!-- Preloader -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="{{ asset('templates/adminlte-3.2.0/dist/img/AdminLTELogo.png') }}"
+                alt="AdminLTELogo" height="60" width="60">
+        </div>
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="index3.html" class="nav-link">Beranda</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item dropdown user-menu">
+
+                </li>
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown user-menu">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                        <img src="{{ asset('templates/adminlte-3.2.0/dist/img/user2-160x160.jpg') }}" class="user-image img-circle elevation-2" alt="User Image">
+                        <span class="d-none d-md-inline">test</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <!-- User image -->
+                        <li class="user-header bg-primary">
+                            <img src="{{ asset('templates/adminlte-3.2.0/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                            <p>test</p>
+                        </li>
+                        <!-- Menu Body -->
+                        <li class="user-body">
+                            <div class="row">
+                                <div class="col-12 text-center">
+                                    <p>test</p>
+                                </div>
+                            </div>
+                            <!-- /.row -->
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <a href="#" class="btn btn-default btn-flat">Profil</a>
+                            <a href="#" class="btn btn-default btn-flat float-right">Keluar</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#"
+                        role="button">
+                        <i class="fas fa-th-large"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- /.navbar -->
+
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link">
+                <img src="{{ asset('templates/adminlte-3.2.0/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">AdminLTE 3</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="{{ asset('templates/adminlte-3.2.0/dist/img/user2-160x160.jpg') }}"
+                            class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">test</a>
+                    </div>
+                </div>
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
+                            with font-awesome or any other icon font library -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">
+                                <i class="nav-icon fas fa-home"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+        <div class="content-wrapper">
+            @yield('content')
+            <!-- jQuery -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/jquery/jquery.min.js') }}"></script>
+            <!-- jQuery UI 1.11.4 -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+            <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+            <script>
+                $.widget.bridge('uibutton', $.ui.button)
+            </script>
+            <!-- Bootstrap 4 -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+            <!-- ChartJS -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/chart.js/Chart.min.js') }}"></script>
+            <!-- Sparkline -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/sparklines/sparkline.js') }}"></script>
+            <!-- JQVMap -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+            <!-- jQuery Knob Chart -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+            <!-- daterangepicker -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/moment/moment.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/daterangepicker/daterangepicker.js') }}"></script>
+            <!-- Tempusdominus Bootstrap 4 -->
+            <script
+                src="{{ asset('templates/adminlte-3.2.0/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}">
+            </script>
+            <!-- Summernote -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/summernote/summernote-bs4.min.js') }}"></script>
+            <!-- overlayScrollbars -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}">
+            </script>
+            <!-- AdminLTE App -->
+            <script src="{{ asset('templates/adminlte-3.2.0/dist/js/adminlte.js') }}"></script>
+            <!-- AdminLTE for demo purposes -->
+            {{-- <script src="{{ asset('templates/adminlte-3.2.0/dist/js/demo.js') }}"></script> --}}
+            <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+            <script src="{{ asset('templates/adminlte-3.2.0/dist/js/pages/dashboard.js') }}"></script>
+            <!-- DataTables  & Plugins -->
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}">
+            </script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}">
+            </script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/jszip/jszip.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/pdfmake/pdfmake.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/pdfmake/vfs_fonts.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+            <script src="{{ asset('templates/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
+        </div>
+    </div>
+</body>
+
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+    </div>
+</footer>
+
+<!-- Control adminlte-3.2.0/ -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-adminlte-3.2.0/ -->
+
+
+</html>
