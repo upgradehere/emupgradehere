33133d0b5fbf7077762c275743488447398f6f91
Naufal Ramadhan <ramnaufal@gmail.com>
2024-11-29 23:42:04 +0700

    migration doctor

---
+++ database/migrations/2024_11_29_233731_doctor_m.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctor_m', function (Blueprint $table) {
+            $table->id();
+            $table->string('doctor_code', 8);
+            $table->string('doctor_name');
+            $table->text('doctor_sign');
+            $table->softDeletes();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
